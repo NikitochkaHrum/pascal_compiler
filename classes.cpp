3f92d4c4838930ca4d8a112dc1b523952655706c
NikitochkaHrum <ponkin.nikitka@mail.ru>
2022-09-27 16:32:54 +0500

    First code

---
+++ classes.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+#include "classes.h"
+
+TextPos::TextPos(int line_number_, int char_number_){
+    line_number = line_number_;
+    char_number = char_number_;
+}
---
+++ classes.h
@@ -0,0 +1,6 @@
+class TextPos
+{
+public:
+    int line_number, char_number;
+    TextPos(int line_number_, int char_number_);
+};
