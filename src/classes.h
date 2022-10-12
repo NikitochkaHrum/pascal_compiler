9e38d83360d24ff4d8dd96d6a0cf80160e456720
NikitochkaHrum <ponkin.nikitka@mail.ru>
2022-10-12 19:24:23 +0500

    some classes for lexer

--- src/classes.h
+++ src/classes.h
@@ -11,3 +11,40 @@ public:
 
 bool operator==(const TextPos a, const TextPos b);
 bool operator!=(const TextPos a, const TextPos b);
+
+enum TokenType {Constant, Identifier, KeyWord};
+enum VarType {Int, Float, String};
+enum KeyWordType {Program, Var, If, For, Else, While, begin, end, Begin, End};
+
+class CToken
+{
+public:
+    TokenType tt;
+    virtual std::string ToString();
+};
+
+class CConstToken : public CToken
+{
+public:
+    VarType value;
+    std::string ToString() override;
+};
+
+class CIdentToken : public CToken
+{
+public:
+    std::string value;
+    std::string ToString() override;
+};
+
+class CKeyWordToken : public CToken
+{
+public:
+    KeyWordType value;
+    std::string ToString() override;
+};
+
+// class CVariant
+// {
+//     VarType vt;
+// };
